9ed061a786f1d95023436da4c7cd7f6ab02ccf81
hrithik-b <hrithikb023@gmail.com>
2023-06-09 12:21:39 +0530

    Commit by Hrithik

--- lib/pages/phone.dart
+++ lib/pages/phone.dart
@@ -5,35 +5,35 @@ import 'otp.dart';
 class MyPhone extends StatefulWidget {
   const MyPhone({Key? key}) : super(key: key);
 
-  static String verify="";
+  static String verify = "";
 
   @override
   State<MyPhone> createState() => _MyPhoneState();
 }
 
 class _MyPhoneState extends State<MyPhone> {
-  TextEditingController countrycode=TextEditingController();
-  var phone="";
+  TextEditingController countrycode = TextEditingController();
+  var phone = "";
   @override
   void initState() {
     // TODO: implement initState
-    countrycode.text="+91";
+    countrycode.text = "+91";
     super.initState();
   }
+
   @override
   Widget build(BuildContext context) {
     final auth = FirebaseAuth.instance;
     return Scaffold(
-     // backgroundColor: Color(0xff0660C6),
-      body:Container(
-        
-        margin: EdgeInsets.only(left: 25,right: 25),
+      // backgroundColor: Color(0xff0660C6),
+      body: Container(
+        margin: EdgeInsets.only(left: 25, right: 25),
         alignment: Alignment.center,
         child: SingleChildScrollView(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-           /* Stack(
+              /* Stack(
               children:<Widget>[Text('SCHOOLWALE',style: TextStyle(
                   fontSize: 40,foreground: Paint()..style=PaintingStyle.stroke
                   ..strokeWidth=6
@@ -41,32 +41,40 @@ class _MyPhoneState extends State<MyPhone> {
               ),),
              Text('SCHOOLWALE',style: TextStyle(fontSize: 40,color: Colors.white),
               ),],),*/
-              const Image(image:AssetImage('assets/images/schoolwale-logo.jpeg') ,width: 250,height: 250,),
+              const Image(
+                image: AssetImage('assets/images/schoolwale-logo.jpeg'),
+                width: 250,
+                height: 250,
+              ),
               SizedBox(
                 height: 0,
               ),
-
-              Text('Phone Verification',style: TextStyle(
-                fontSize: 22,fontWeight: FontWeight.bold,
-              ),),
+              Text(
+                'Phone Verification',
+                style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
               SizedBox(
                 height: 10,
               ),
-              Text('We need to register your phone before getting started',style: TextStyle(
-              fontSize: 16,
-              ),
+              Text(
+                'We need to register your phone before getting started',
+                style: TextStyle(
+                  fontSize: 16,
+                ),
                 textAlign: TextAlign.center,
               ),
               SizedBox(
                 height: 30,
               ),
               Container(
-                height:55,
+                height: 55,
                 decoration: BoxDecoration(
-                    border: Border.all(width: 1,color: Colors.grey),
-                    borderRadius: BorderRadius.circular(10)
-                ),
-                child:  Row(
+                    border: Border.all(width: 1, color: Colors.grey),
+                    borderRadius: BorderRadius.circular(10)),
+                child: Row(
                   children: [
                     SizedBox(
                       width: 10,
@@ -75,70 +83,69 @@ class _MyPhoneState extends State<MyPhone> {
                       width: 40,
                       child: TextField(
                         controller: countrycode,
-                        decoration: InputDecoration(
-                            border: InputBorder.none),
+                        decoration: InputDecoration(border: InputBorder.none),
                       ),
                     ),
-
-                    Text("|",style: TextStyle(
-                        fontSize: 33,color: Colors.grey
-                    ),),
+                    Text(
+                      "|",
+                      style: TextStyle(fontSize: 33, color: Colors.grey),
+                    ),
                     SizedBox(
                       width: 10,
                     ),
                     Expanded(
                       child: TextField(
                         keyboardType: TextInputType.phone,
-                        onChanged: (value){
-                          phone=value;
-                      },
-
+                        onChanged: (value) {
+                          phone = value;
+                        },
                         decoration: InputDecoration(
-                            border: InputBorder.none,hintText: "Phone"
-                        ),
-
+                            border: InputBorder.none, hintText: "Phone"),
                       ),
                     ),
-
                   ],
                 ),
               ),
-
               SizedBox(
-             height: 20,
-           ),
-             SizedBox(
-               height: 45,
-               width:300,
-               child: ElevatedButton(onPressed: () async{
-                 await auth.verifyPhoneNumber(
-                   phoneNumber:'+91 95916 19204',
-                   verificationCompleted:  (PhoneAuthCredential credential) async {
-                     // ANDROID ONLY!
+                height: 20,
+              ),
+              SizedBox(
+                height: 45,
+                width: 300,
+                child: ElevatedButton(
+                  onPressed: () async {
+                    await auth.verifyPhoneNumber(
+                      phoneNumber: '+91 6238 697 724',
+                      verificationCompleted:
+                          (PhoneAuthCredential credential) async {
+                        // ANDROID ONLY!
 
-                     // Sign the user in (or link) with the auto-generated credential
-                     await auth.signInWithCredential(credential);
-                   },
-                   verificationFailed: (FirebaseAuthException e) {
-                       if (e.code == 'invalid-phone-number') {
-                         print('The provided phone number is not valid.');
-                       }
+                        // Sign the user in (or link) with the auto-generated credential
+                        await auth.signInWithCredential(credential);
+                      },
+                      verificationFailed: (FirebaseAuthException e) {
+                        if (e.code == 'invalid-phone-number') {
+                          print('The provided phone number is not valid.');
+                        }
 
-                       // Handle other errors
-
-                   },
-                   codeSent: (String verificationId, int? resendToken) {
-                     MyPhone.verify=verificationId;
-                     Navigator.push(context,MaterialPageRoute(builder: (context)=>MyOtp()));
-                   },
-                   timeout: const Duration(seconds: 60),
-                   codeAutoRetrievalTimeout: (String verificationId) {},
-                 );
-               }, child: Text('Send the Code'),style: ElevatedButton.styleFrom(
-                   primary: Color(0xff0660C6),shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10))
-               ),) ,
-             )
-
+                        // Handle other errors
+                      },
+                      codeSent: (String verificationId, int? resendToken) {
+                        MyPhone.verify = verificationId;
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (context) => MyOtp()));
+                      },
+                      timeout: const Duration(seconds: 60),
+                      codeAutoRetrievalTimeout: (String verificationId) {},
+                    );
+                  },
+                  child: Text('Send the Code'),
+                  style: ElevatedButton.styleFrom(
+                      primary: Color(0xff0660C6),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10))),
+                ),
+              )
             ],
           ),
         ),
