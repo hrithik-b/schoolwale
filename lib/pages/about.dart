d6db84bc56b9d9b2c2e9f22338a3ae12bf49e2df
HARSHINI <harshpoojary178@gmail.com>
2023-06-27 15:04:58 +0530

    commit

--- lib/pages/about.dart
+++ lib/pages/about.dart
@@ -1,138 +1,65 @@
-import 'dart:ui';
-import 'homepage.dart';
-import 'package:flutter/widgets.dart';
+
 import 'package:flutter/material.dart';
-import 'package:schoolwale/widgets/aboutContainer.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import '../widgets/aboutContainer.dart';
 
-class MyAbout extends StatefulWidget {
-  const MyAbout({super.key});
-
-  @override
-  State<MyAbout> createState() => _MyAboutState();
-}
-
-class _MyAboutState extends State<MyAbout> {
+class About extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Color(0xff0660C6),
         leading: IconButton(
-          onPressed: () => {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => const MyHomePage()),
-            )
+          onPressed: () {
+            Navigator.pop(context);
           },
           icon: Icon(Icons.arrow_back_ios),
         ),
         title: Text(
-          "About School",
-          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          "About",
+          style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
         ),
         centerTitle: true,
       ),
-      body: Center(
-          child: SingleChildScrollView(
-        child: Column(
-          children: [
-            Text(
-              "\t\t\t\t\tBIT",
-              style: TextStyle(
-                  color: Colors.black87,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 34),
-            ),
-            SizedBox(
-              height: ((MediaQuery.of(context).size.height) / 10) * 0.3,
-            ),
-            Container(
-              decoration: BoxDecoration(
-                color: Colors.white,
-                shape: BoxShape.circle,
-                boxShadow: [
-                  BoxShadow(
-                    color: Color.fromRGBO(218, 201, 201, 1),
-                    blurRadius: 6.0,
-                    spreadRadius: 6.0,
-                    offset: Offset(0.0, 0.0),
-                  )
-                ],
-              ),
-              child: CircleAvatar(
-                backgroundImage: AssetImage('assets/images/schoolPicture.jpg'),
-                radius: 120,
-              ),
-            ),
-            SizedBox(
-              // height:((MediaQuery.of(context).size.height)/10)*1,
-              height: 20,
-              // height: 20,
-            ),
-            Container(
-              alignment: Alignment.center,
-              width: (MediaQuery.of(context).size.width) / 10 * 9,
-              height: (MediaQuery.of(context).size.height) / 10 * 4.7,
-              decoration: BoxDecoration(
-                boxShadow: [
-                  BoxShadow(
-                    color: Color.fromRGBO(218, 201, 201, 1),
-                    blurRadius: 6.0,
-                    spreadRadius: 6.0,
-                    offset: Offset(0.0, 0.0),
-                  )
-                ],
-                color: Color.fromRGBO(221, 215, 245, 0.302),
-                borderRadius: BorderRadius.circular(30),
-              ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                      "Our school is a vibrant learning community dedicated to fostering academic excellence and personal growth. With dedicated teachers, state-of-the-art facilities, and a supportive environment, we provide a well-rounded education that prepares students for success in the modern world. Through engaging curriculum, extracurri -cular activities, and a focus on character development, we strive to empower our students to become lifelong learners and responsible global citizens.",
-                      style: TextStyle(
-                        fontSize: 18,
-                        wordSpacing: 3,
-                        letterSpacing: 1,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            SizedBox(
-              height: 2,
-            ),
-            ContainerWidget(
-              cwidth: (MediaQuery.of(context).size.width) / 10 * 9,
-              cheight: ((MediaQuery.of(context).size.height) / 10) * 3.4,
-              heading: "Our Mission",
-              content:
-                  "Our mission is to provide a nurturing and inclusive learning environment where students can thrive academically, socially, and emotionally. We aim to inspire a love for learning, cultivate critical thinking skills, and foster a strong sense of character and community.",
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            ContainerWidget(
-              cwidth: (MediaQuery.of(context).size.width) / 10 * 9,
-              cheight: ((MediaQuery.of(context).size.height) / 10) * 5.7,
-              heading: "Achievements",
-              content:
-                  "Our school takes pride in its outstanding academic achievements, consistently surpassing national standards and earning recognition for excellence in various subject areas. We have a strong track record of preparing students for higher education, with a high percentage of graduates being accepted into top-tier universities. Additionally, our school has received accolades for its inclusive and diverse community, fostering a supportive environment where every student can thrive.",
-            ),
-            SizedBox(
-              // height:((MediaQuery.of(context).size.height)/10)*1,
-              height: 20,
-              // height: 20,
-            ),
-          ],
-        ),
-      )),
+      body: FutureBuilder(
+        future: FirebaseFirestore.instance.collection("About").get(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError || snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          final data = snapshot.data!.docs;
+          final aboutItems = data.map((doc) => doc.data()).toList();
+          return AboutPageContent(aboutItems: aboutItems);
+        },
+      ),
+    );
+  }
+}
+
+class AboutPageContent extends StatelessWidget {
+  final List<Map<String, dynamic>> aboutItems;
+
+  const AboutPageContent({Key? key, required this.aboutItems}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: aboutItems.length,
+      itemBuilder: (context, index) {
+        final item = aboutItems[index];
+        return AboutContainer(
+          heading: item['heading'],
+          content:item['content'],
+          imageUrl: item['imageUrl'],
+          header:item ['header'],
+          missioncontent: item['missioncontent'],
+          missionhead: item['missionhead'],
+
+
+        );
+      },
     );
   }
 }
--- lib/widgets/aboutContainer.dart
+++ lib/widgets/aboutContainer.dart
@@ -1,61 +1,142 @@
 import 'package:flutter/material.dart';
-class ContainerWidget extends StatelessWidget {
-    final String heading;
-    final cwidth;
-    final cheight;
-    final String content;
-  const ContainerWidget({super.key, required this.heading, this.cwidth, this.cheight, required this.content});
+
+class AboutContainer extends StatelessWidget {
+  final String heading;
+  final String content;
+  final String header;
+  final String imageUrl;
+  final String missionhead;
+  final String missioncontent;
+
+  const AboutContainer({
+    Key? key,
+    required this.heading,
+    required this.content,
+    required this.header,
+    required this.imageUrl,
+    required this.missioncontent,
+    required this.missionhead,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
-              alignment: Alignment.center,
-             width:cwidth,//(MediaQuery.of(context).size.width)/10*9,
-             height:cheight,//((MediaQuery.of(context).size.height)/10)*5.5,
-              decoration: BoxDecoration(
-                boxShadow: [
-                BoxShadow(
-                  color: Color.fromRGBO(218, 201, 201, 1),
-                  blurRadius: 6.0,
-                  spreadRadius: 6.0,
-                  offset: Offset(0.0, 0.0),
-                )
-              ],
-                color: Color.fromRGBO(221, 215, 245, 0.302),
-                borderRadius: BorderRadius.circular(30),
+    return Column(
+      children: [
+        // Check if the header is provided
+        Container(
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+          alignment: Alignment.center,
+          child: Text(
+            header,
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        // Check if the image URL is provided
+        Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            shape: BoxShape.circle,
+            boxShadow: [
+              BoxShadow(
+                color: Color.fromRGBO(218, 201, 201, 1),
+                blurRadius: 6.0,
+                spreadRadius: 6.0,
+                offset: Offset(0.0, 0.0),
               ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                      heading,
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                        wordSpacing: 3,
-                        letterSpacing: 1,
-                      ),
-                    ),
-                  ),
-                  Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text(
-                        content,
-                        style: TextStyle(
-                          fontSize: 18,
-                          wordSpacing: 3,
-                          letterSpacing: 1,
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
+            ],
+          ),
+          child: CircleAvatar(
+            backgroundImage: NetworkImage(imageUrl),
+            radius: 120,
+          ),
+        ),
+        Container(
+          alignment: Alignment.center,
+          width: double.infinity,
+          padding: const EdgeInsets.all(16),
+          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+          decoration: BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+                color: Color.fromRGBO(218, 201, 201, 1),
+                blurRadius: 6.0,
+                spreadRadius: 6.0,
+                offset: Offset(0.0, 0.0),
               ),
-          
+            ],
+            color: Color.fromRGBO(221, 215, 245, 0.302),
+            borderRadius: BorderRadius.circular(30),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                heading,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 8),
+              Text(
+                content,
+                style: TextStyle(
+                  fontSize: 18,
+                  wordSpacing: 3,
+                  letterSpacing: 1,
+                ),
+              ),
+            ],
+          ),
+        ),
+        Container(
+          alignment: Alignment.center,
+          width: double.infinity,
+          padding: const EdgeInsets.all(16),
+          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+          decoration: BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+                color: Color.fromRGBO(218, 201, 201, 1),
+                blurRadius: 6.0,
+                spreadRadius: 6.0,
+                offset: Offset(0.0, 0.0),
+              ),
+            ],
+            color: Color.fromRGBO(221, 215, 245, 0.302),
+            borderRadius: BorderRadius.circular(30),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                missionhead,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 8),
+              Text(
+                missioncontent,
+                style: TextStyle(
+                  fontSize: 18,
+                  wordSpacing: 3,
+                  letterSpacing: 1,
+                ),
+              ),
+            ],
+          ),
+        ),
+
+    ],
     );
   }
 }
