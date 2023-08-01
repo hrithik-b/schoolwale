f14d12df2bbaea0165ed3de72869d52d86612d66
veekshitha10 <veekshithabangera10@gmail.com>
2023-08-01 12:29:50 +0530

    commit

--- lib/Models/Announcements_converter.dart
+++ lib/Models/Announcements_converter.dart
@@ -1,20 +1,23 @@
 class AnnouncementConverter{
   final String title;
   final String date;
+  final scheduled_date;
   final String description;
 
   AnnouncementConverter({
     required this.title,
     required this.date,
     required this.description,
+    required this.scheduled_date,
   });
 
   // Factory constructor to create an Announcement object from JSON data
   factory AnnouncementConverter.fromJson(Map<String, dynamic> json) {
     return AnnouncementConverter(
       title: json['AnnName'] as String,
-      date: json['AnnDate'] as  String,
-      description: json['CreateDate'] as String,
+      scheduled_date: json['scheduledDate'] ,
+      description: json['description'] as String,
+      date: json['CreateDate'] as String,
     );
   }
 }
--- lib/widgets/announcement_items.dart
+++ lib/widgets/announcement_items.dart
@@ -1,24 +1,32 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class annoucementsUpdate extends StatelessWidget {
   // final IconData icon;
   final String Annoucementname;
-  final String Annocementsdate;
+  final Annocementsdate;
   final String AnnocementsUpdateddate;
-  const annoucementsUpdate({Key? key, required this.Annoucementname, required this.Annocementsdate, required this.AnnocementsUpdateddate, }) : super(key: key);
+  final String AnnouncementDescription;
+  const annoucementsUpdate(
+      {Key? key,
+      required this.Annoucementname,
+      required this.Annocementsdate,
+      required this.AnnocementsUpdateddate,
+      required this.AnnouncementDescription})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      child:Card(
+      child: Card(
         elevation: 8,
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(20),
         ),
         color: Colors.lightBlue[50],
         child: Container(
-          
-          height: 150,
+          height: 300,
           width: 200,
           alignment: Alignment.center,
           child: Column(
@@ -29,7 +37,7 @@ class annoucementsUpdate extends StatelessWidget {
               //   color: Color.fromARGB(255, 0, 101, 184),
               // ),
               Text(
-              Annoucementname,
+                Annoucementname,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 24,
@@ -37,7 +45,18 @@ class annoucementsUpdate extends StatelessWidget {
                 ),
               ),
               Text(
-                Annocementsdate,
+                AnnouncementDescription,
+                //textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+
+              Text(
+                DateFormat.yMd()
+                        .add_jm()
+                        .format((Annocementsdate as Timestamp).toDate()) ??'',
                 textAlign: TextAlign.left,
               ),
               Text(
@@ -48,7 +67,6 @@ class annoucementsUpdate extends StatelessWidget {
           ),
         ),
       ),
-
     );
   }
 }
